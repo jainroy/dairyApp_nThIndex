adc4da2291fcb075423389a07ea31125202c8db3
jain <jainroyp@gmail.com>
2025-07-24 16:34:39 +0530

    latest commit

--- authentication/models.py
+++ authentication/models.py
@@ -18,23 +18,23 @@ class UserManager(BaseUserManager):
         user.save()
         return user
 
-    # def create_superuser(self, username, email, password=None):
+    def create_superuser(self, username, email, password=None):
 
-    #     if password is None:
-    #         raise TypeError('Password should not be none.')
+        if password is None:
+            raise TypeError('Password should not be none.')
         
-    #     user = self.create_user(username, email, password)
-    #     user.is_superuser = True
-    #     user.is_staff = True
-    #     user.save()
-    #     return user
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=150, unique=True, db_index=True)
     email = models.EmailField(max_length=255, unique=True, db_index=True)
-    # is_verified = models.BooleanField(default=False)
-    # is_active = models.BooleanField(default=True)
-    # is_staff = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)    
     updated_at = models.DateTimeField(auto_now=True)    
 
--- diaryProject/settings.py
+++ diaryProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c3v@1@l8x6w2yq2v$c6x6i3o2s%161=e0%5^p&%7uztu&eq(ne
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 AUTH_USER_MODEL = 'authentication.User'
 
 REST_FRAMEWORK = {
